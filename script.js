aa28e5941b6c89b5af15ab0d524c4649c1206131
John Werner <jwerner547@gmail.com>
2019-10-25 20:06:05 -0600

    it's not everything that I thought it would be, but I am pretty happy with it

--- script.js
+++ script.js
@@ -2,8 +2,35 @@ let app = new Vue({
     el: '#app',
     data: {
         cards: [],
-        loading: true,
+        loading: 2,
         colorFilters: [],
+        typeFilters: [],
+        rarityFilters: [],
+        colors: [
+            "White",
+            "Green",
+            "Red",
+            "Black",
+            "Blue"
+        ],
+        cardTypes: [
+            "Creature",
+            "Instant",
+            "Sorcery",
+            "Enchantment",
+            "Planeswalker",
+            "Artifact",
+            "Land"
+        ],
+        rarities: [
+            "Common",
+            "Uncommon",
+            "Rare",
+            "Mythic Rare",
+            "Special",
+            "Basic Land"
+        ],
+        expanded: false
     },
     created() {
         // this.getCards();
@@ -11,15 +38,30 @@ let app = new Vue({
     methods: {
         async getCards() {
             try {
-                let url = 'https://api.magicthegathering.io/v1/cards';
+                let url = 'https://api.magicthegathering.io/v1/cards?';
                 if (this.colorFilters.length > 0) {
-                    url += '?colors=' + this.colorFilters.join();
+                    if (url.charAt(url.length - 1) != '?') {
+                        url += '&'
+                    }
+                    url += 'colors=' + this.colorFilters.join('|').toLowerCase();
                 }
-                this.loading = true;
+                if (this.typeFilters.length > 0) {
+                    if (url.charAt(url.length - 1) != '?') {
+                        url += '&'
+                    }
+                    url += 'types=' + this.typeFilters.join('|').toLowerCase();
+                }
+                if (this.rarityFilters.length > 0) {
+                    if (url.charAt(url.length - 1) != '?') {
+                        url += '&'
+                    }
+                    url += 'rarities=' + this.rarityFilters.join('|').toLowerCase();
+                }
+                this.loading = 1;
                 const response = await axios.get(url);
                 this.cards = response.data.cards
-                console.log(this.cards);
-                this.loading = false;
+                console.log(this.cards)
+                this.loading = 0;
             } catch (error) {
                 console.log(error);
             }
@@ -30,6 +72,34 @@ let app = new Vue({
             } else {
                 this.colorFilters.push(color);
             }
+        },
+        toggleTypeFilter(type) {
+            if (this.typeFilters.includes(type)) {
+                this.typeFilters.splice(this.typeFilters.indexOf(type), 1);
+            } else {
+                this.typeFilters.push(type);
+            }
+        },
+        toggleRarityFilter(r) {
+            if (this.rarityFilters.includes(r)) {
+                this.rarityFilters.splice(this.rarityFilters.indexOf(r), 1);
+            } else {
+                this.rarityFilters.push(r);
+            }
+        },
+        getColorSVG(c) {
+            return "./images/svgs/" + c + ".svg";
+        },
+        toggleFilters() {
+            var f = document.getElementById("filters");
+
+            if (f.style.display === "block") {
+                f.style.display = "none";
+            } else {
+                f.style.display = "block";
+            }
+
+            this.expanded = !this.expanded;
         }
     }
 })
